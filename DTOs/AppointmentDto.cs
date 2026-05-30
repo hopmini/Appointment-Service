cac2d169edd7a3ef7bd450faefc8c653644378c2
hopmini <hop13101999@gmail.com>
2026-05-30 22:48:51 +0700

    update

---
+++ DTOs/AppointmentDto.cs
@@ -0,0 +1,31 @@
+#nullable enable
+namespace AppointmentService.DTOs
+{
+    // DTO để gọi lệnh tự động đẻ ra slot
+    public class GenerateSlotDto
+    {
+        public Guid DoctorId { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    // DTO khi bệnh nhân gửi yêu cầu đặt lịch
+    public class BookAppointmentDto
+    {
+        public int PatientId { get; set; } // Bắt buộc phải đăng nhập
+        public Guid SlotId { get; set; }    // Slot vừa chọn
+        public Guid MedicalServiceId { get; set; } // Dịch vụ khám chọn từ danh sách
+        public string Reason { get; set; } = string.Empty;  // Triệu chứng nhức đầu sổ mũi...
+    }
+
+    // DTO để cập nhật lý do khám bệnh
+    public class UpdateReasonDto
+    {
+        public string Reason { get; set; } = string.Empty;
+    }
+
+    // DTO để đổi lịch hẹn (đổi Slot)
+    public class RescheduleDto
+    {
+        public Guid NewSlotId { get; set; }
+    }
+}
